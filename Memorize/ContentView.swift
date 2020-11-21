22bcb796a4c8602e82b2d0c4633f157553c7aeaa
Jeevan James <jeeves512@gmail.com>
2020-11-21 10:35:31 +0000

    HStack implemented

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -9,8 +9,18 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        return HStack(){
+            ForEach(0..<4){ index in
+                ZStack(){
+                    RoundedRectangle(cornerRadius: 10.0).fill(Color.white)
+                    RoundedRectangle(cornerRadius: 10.0).stroke(lineWidth: 3)
+                    Text("💡")
+                }
+            }
+        }
+        .padding()
+        .foregroundColor(Color.orange)
+        .font(Font.largeTitle)
     }
 }
 
