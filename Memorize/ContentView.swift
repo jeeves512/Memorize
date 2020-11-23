6920f1f8399f479776ad3b992dfcf32db86e8885
Jeevan James <jeeves512@gmail.com>
2020-11-23 18:12:07 +0000

    Added Model and ModelView

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -8,13 +8,12 @@
 import SwiftUI
 
 struct ContentView: View {
+    var viewModel: EmojiMemoryGame
     var body: some View {
         return HStack(){
-            ForEach(0..<4){ index in
-                ZStack(){
-                    RoundedRectangle(cornerRadius: 10.0).fill(Color.white)
-                    RoundedRectangle(cornerRadius: 10.0).stroke(lineWidth: 3)
-                    Text("💡")
+            ForEach(viewModel.cards){ card in
+                CardView(card: card).onTapGesture{
+                    viewModel.choose(card: card)
                 }
             }
         }
@@ -24,8 +23,23 @@ struct ContentView: View {
     }
 }
 
+struct CardView: View {
+    var card: MemoryGame<String>.Card
+    var body: some View {
+        ZStack(){
+            if card.isFaceUp{
+                RoundedRectangle(cornerRadius: 10.0).fill(Color.white)
+                RoundedRectangle(cornerRadius: 10.0).stroke(lineWidth: 3)
+                Text(card.content)
+            }else{
+                RoundedRectangle(cornerRadius: 10.0).fill()
+            }
+        }
+    }
+}
+
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ContentView(viewModel: EmojiMemoryGame())
     }
 }
---
+++ Memorize/EmojiMemoryGame.swift
@@ -0,0 +1,31 @@
+//
+//  EmojiMemoryGame.swift
+//  Memorize
+//
+//  Created by Jeevan James on 23/11/2020.
+//
+
+import Foundation
+import SwiftUI
+
+class EmojiMemoryGame {
+    private var model: MemoryGame<String> = EmojiMemoryGame.createMemoryGame()
+    
+    static func createMemoryGame() -> MemoryGame<String> {
+        let emojis = ["👻","🎃","🕷"]
+        return MemoryGame<String>(numberOfPairsOfCards: emojis.count) { pairIndex in
+            return emojis[pairIndex]
+        }
+    }
+    
+    // MARK: - Access to model
+    var cards: Array<MemoryGame<String>.Card>{
+        model.cards
+    }
+    
+    // MARK: - Intent
+    
+    func choose(card: MemoryGame<String>.Card){
+        model.choose(card: card)
+    }
+}
